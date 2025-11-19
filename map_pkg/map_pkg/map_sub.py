2ee8389bd7e42cc89be64881bfb25ca137285753
Kianna Ng <55515793+kiannang@users.noreply.github.com>
2025-11-18 18:30:29 -0800

    Update map_sub.py

--- map_pkg/map_pkg/map_sub.py
+++ map_pkg/map_pkg/map_sub.py
@@ -1,19 +1,21 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from geometry_msgs.msg import Pose2D
 
 class MapSubscriber(Node):
     def __init__(self):
         super().__init__('map_sub')
         self.subscription = self.create_subscription(
-            String,
+            Pose2D,
             'map_topic',
             self.listener_callback,
             10
         )
 
     def listener_callback(self, msg):
-        self.get_logger().info(f"Received: {msg.data}")
+        self.get_logger().info(
+            f"Received Pose2D: x={msg.x:.2f}, y={msg.y:.2f}, theta={msg.theta:.1f}"
+        )
 
 def main(args=None):
     rclpy.init(args=args)
